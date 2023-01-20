0f1aa3350ecc0df70f7fdeeeb13be9f51c12b7f3
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-01-20 17:20:11 +0100

    Clean files

--- references.R
+++ references.R
@@ -1,2 +1,3 @@
 # Créer automatiquement une bibliographie des packages R utilisés
-knitr::write_bib(c(.packages(), desc::desc_get_deps()$package), 'packages.bib')
+dep <- desc::desc_get_deps()$package[-1] # Retirer R base
+knitr::write_bib(c(.packages(), dep), file = "packages.bib", prefix = "R-")
